9cfe17cfb5dfbe2bf5408b02e7932f70f76bae97
Ratan kadam <ratan.kadam@gmail.com>
2016-02-17 14:36:15 -0800

    Updating
    Updating  latest info

--- web-tutorials/portfolioWebsite/public/Jquery/ratanJquery.js
+++ web-tutorials/portfolioWebsite/public/Jquery/ratanJquery.js
@@ -1,31 +1,53 @@
-$(document).ready(function(){
-	
-	   $("#projGame").click(function(){
-		   $("#myModal").modal('show');
-		   
-	   });
-	   
-	   $("#proj2").click(function(){
-		   
-		$("#myModal1").modal('show');   
-		   
-	   });
-	   
-	   $("#proj3").click(function(){
-		   
-			$("#myModal2").modal('show');   
-			   
-		   });
-	   
-	   $("#proj4").click(function(){
-		   $("#myModal3").modal('show');
-	   });
-	   
-	   $("#proj5").click(function(){
-		   $("#myModal4").modal('show');
-	   });
-	   
-	   
-	   
+$(document).ready(function() {
+
+	$("#projGame,#myProject1").click(function() {
+		$("#myModal").modal('show');
+
+	});
+
+	$("#proj2,#myProject2").click(function() {
+
+		$("#myModal1").modal('show');
+
+	});
+
+	$("#proj3,#myProject3").click(function() {
+
+		$("#myModal2").modal('show');
+
+	});
+
+	$("#proj4,#myProject4").click(function() {
+		$("#myModal3").modal('show');
+	});
+
+	$("#proj5,#myProject5").click(function() {
+		$("#myModal4").modal('show');
+	});
+
+	$("#proj6,#myProject6").click(function() {
+		$("#myModal6").modal('show');
+	});
+
+	$("#proj7,#myProject7").click(function() {
+		$("#myModal7").modal('show');
+	});
+
+	// for showiung more projects
+
+	$("#showMore").click(function() {
+
+		$("#rowThree").addClass("showMe"); // showing new projects..
+		$(this).removeClass("showMe"); // removing showMore button;
+		$(this).addClass("hideMe");
+		$("#showLess").addClass("showMe"); // displaying ShowLess button.
+
+	});
+
+	$("#showLess").click(function() {
+		$("#rowThree").removeClass("showMe"); // hiding new projects
+		$(this).removeClass("showMe"); // removing showLess button
+		$("#showMore").addClass("showMe"); // displaying showmore button
+	});
+
 });
-	
